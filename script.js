5e17cc8b0f8c269b6f0ff6535edb113e1318859f
clau10cls <clau10.dcls@gmail.com>
2023-04-05 16:16:35 -0300

    Funcion para botones
    Unificacion de la funcion de los botones del formulario de proceso.

--- script.js
+++ script.js
@@ -1,123 +1,53 @@
 // CONTENEDOR DEL FORMULARIO
 
-const containerMsf = document.querySelector('#container-msf');
+const msfContainer = document.querySelector('#msf-container');
 
-/* 
+// PASOS FORMULARIO
 
-const msfStep1 = containerMsf.querySelector('#msf-step1');
-const msfStep2 = containerMsf.querySelector('#msf-step2');
-const msfStep3 = containerMsf.querySelector('#msf-step3');
-const msfStep4 = containerMsf.querySelector('#msf-step4'); 
+const msfContent = msfContainer.querySelectorAll('.msf-content');
 
-*/
+// BOTONES 
 
-// ARRAY DE PASOS FORMULARIO
+const msfBtnsNext = msfContainer.querySelectorAll('.msf-btn-next');
+const msfBtnsPrev = msfContainer.querySelectorAll('.msf-btn-prev');
+const msfBtnSubmit = msfContainer.querySelector('#msf-btn_submit');
 
-const contentmsf = containerMsf.querySelectorAll('.content-msf');
+// FUNCION BOTON ANTERIOR
 
-// BOTONES
+for (let h = 0; h < msfBtnsPrev.length ; h++){
+    msfBtnsPrev[h].addEventListener('click',()=>{
+        for (let i = 0; i < msfContent.length; i++){
+            if(msfContent[i].classList.contains('msf_active')){
+                msfContent[i].style = 'margin-left: 100%';
+                msfContent[i].classList.toggle('msf_active');
+                msfContent[i].classList.toggle('msf_inactive');
+                i--;
+                msfContent[i].style = 'margin-left: 0';
+                msfContent[i].classList.toggle('msf_active');
+                msfContent[i].classList.toggle('msf_inactive');
+            };
+        };
+    });
+};
 
-const msf1BtnNext = containerMsf.querySelector('#msf1-btn_next')
+// FUNCION BOTON SIGUIENTE
 
-const msf2BtnPrev = containerMsf.querySelector('#msf2-btn_prev')
-const msf2BtnNext = containerMsf.querySelector('#msf2-btn_next')
+for (let h = 0; h < msfBtnsNext.length ; h++){
+    msfBtnsNext[h].addEventListener('click',()=>{
+        for (let i = 0; i < msfContent.length; i++){
+            if(msfContent[i].classList.contains('msf_active')){
+                msfContent[i].style = 'margin-left: -100%';
+                msfContent[i].classList.toggle('msf_active');
+                msfContent[i].classList.toggle('msf_inactive');
+                i++;
+                msfContent[i].style = 'margin-left: 0';
+                msfContent[i].classList.toggle('msf_active');
+                msfContent[i].classList.toggle('msf_inactive');
+            };
+        };
+    });
+};
 
-const msf3BtnPrev = containerMsf.querySelector('#msf3-btn_prev')
-const msf3BtnNext = containerMsf.querySelector('#msf3-btn_next')
+// FUNCION BOTON SUBMIT
 
-const msf4BtnPrev = containerMsf.querySelector('#msf4-btn_prev')
-const msf4BtnSubmit = containerMsf.querySelector('#msf4-btn_submit')
-
-// FUNCION BOTONES PASO 1
-
-msf1BtnNext.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: -100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i++;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-// FUNCION BOTONES PASO 2
-
-msf2BtnPrev.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: 100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i--;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-msf2BtnNext.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: -100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i++;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-// FUNCION BOTONES PASO 3
-
-msf3BtnPrev.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: 100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i--;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-msf3BtnNext.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: -100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i++;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-// FUNCION BOTONES PASO 4
-
-msf4BtnPrev.addEventListener('click',()=>{
-    for (let i = 0; i < contentmsf.length; i++){
-        if(contentmsf[i].classList.contains('msf_active')){
-            contentmsf[i].style = 'margin-left: 100%';
-            contentmsf[i].classList.remove('msf_active');
-            contentmsf[i].classList.add('msf_inactive');
-            i--;
-            contentmsf[i].style = 'margin-left: 0';
-            contentmsf[i].classList.add('msf_active');
-            contentmsf[i].classList.remove('msf_inactive');
-        }
-    };
-})
-
-msf4BtnSubmit.addEventListener('click',()=> alert('Datos cargados correctamente!'));
+msfBtnSubmit.addEventListener('click',()=> alert('Datos cargados correctamente!'));
